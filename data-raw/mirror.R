86e285690618803378276681ae163b67bcf57059
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-11-15 13:50:37 +0800

    Update mirror code and test it

--- data-raw/mirror.R
+++ data-raw/mirror.R
@@ -1,6 +1,18 @@
+#!/usr/bin/env Rscript
 ## code to mirror all raw datasets (which can be downloaded by users) of UCSC Xena
-library(UCSCXenaTools)
+
+# Update to the recent UCSCXenaTools
+# BiocManager::install("UCSCXenaTools")
+
+suppressMessages(library(UCSCXenaTools))
 destdir <- commandArgs(trailingOnly = TRUE)
+
+if (length(destdir) != 1) {
+    message("Error: only one directory can be set!")
+    message()
+    message("Usage: ./mirror.R <destdir>")
+    quit("no", status = 1)
+}
 
 download_dataset <- function(x, destdir) {
     x %>%
@@ -10,17 +22,21 @@ download_dataset <- function(x, destdir) {
             destdir = destdir,
             download_probeMap = TRUE,
             trans_slash = FALSE,
-            method = "curl", extra = "-C -") # d断点续传
+            method = "curl", extra = "-C -") # 断点续传
 }
 
-access_datasets <- XenaDataUpdate(saveTolocal = FALSE)
+access_datasets <- XenaData[c(2,4,8),] # XenaDataUpdate(saveTolocal = FALSE)
 hubs <- unique(access_datasets$XenaHostNames)
-lapply(hubs, function(h) {
+dataset_list <- lapply(hubs, function(h) {
     x <- subset(access_datasets, XenaHostNames == h)
     message("Mirroring hub: ", h)
     message("================================")
-    download_dataset()
+    download_dataset(x, file.path(destdir, h))
 })
+mapping_df <- Reduce(rbind, dataset_list)
 
+write.csv(mapping_df,
+          file = file.path(destdir, "mapping_dataframe.csv"),
+          row.names = FALSE)
 message("================================")
 message("The mirror work has done, please check the log.")
