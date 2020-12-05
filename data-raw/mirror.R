a4c3826fb550c19d8f43953626f97d06965df551
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-12-05 23:09:45 +0800

    Update mirror.R

--- data-raw/mirror.R
+++ data-raw/mirror.R
@@ -5,14 +5,22 @@
 # BiocManager::install("UCSCXenaTools")
 
 suppressMessages(library(UCSCXenaTools))
-destdir <- commandArgs(trailingOnly = TRUE)
+library(furrr)
 
-if (length(destdir) != 1) {
-    message("Error: only one directory can be set!")
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 2) {
+    message("Error: two arguments are required!")
     message()
-    message("Usage: ./mirror.R <destdir>")
+    message("Usage: ./mirror.R <destdir> <thread>")
     quit("no", status = 1)
 }
+
+destdir <- path.expand(args[1])
+threads <- args[2]
+
+if (!dir.exists(destdir)) dir.create(destdir, recursive = TRUE)
+future::plan(multisession, workers = threads)
 
 download_dataset <- function(x, destdir) {
     x %>%
@@ -25,18 +33,26 @@ download_dataset <- function(x, destdir) {
             method = "curl", extra = "-C -") # 断点续传
 }
 
-access_datasets <- XenaData # XenaDataUpdate(saveTolocal = FALSE)
+access_datasets <- XenaData[1:10] # XenaDataUpdate(saveTolocal = FALSE)
 hubs <- unique(access_datasets$XenaHostNames)
-dataset_list <- lapply(hubs, function(h) {
+
+sink(file = file.path(destdir, "mirror.log"))
+dataset_list <- furrr::future_map(hubs, function(h) {
+    sink(file = file.path(destdir, "mirror.log"), append = TRUE)
     x <- subset(access_datasets, XenaHostNames == h)
     message("Mirroring hub: ", h)
     message("================================")
     download_dataset(x, file.path(destdir, h))
-})
+},
+.progress = TRUE
+)
+
 mapping_df <- Reduce(rbind, dataset_list)
+
+message("================================")
+message("The mirror work has done, please check the log.")
+sink()
 
 write.csv(mapping_df,
           file = file.path(destdir, "mapping_dataframe.csv"),
           row.names = FALSE)
-message("================================")
-message("The mirror work has done, please check the log.")
