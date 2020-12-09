1ffc981f348a2d280048ea60fd12e24c91d591cd
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-12-09 14:35:27 +0800

    增加顺序执行

--- data-raw/mirror.R
+++ data-raw/mirror.R
@@ -13,18 +13,25 @@ if (length(args) != 2) {
     message("Error: two arguments are required!")
     message()
     message("Usage: ./mirror.R <destdir> <thread>")
+    message("Set thread to 0 to run sequentially.")
     quit("no", status = 1)
 }
 
 destdir <- path.expand(args[1])
 threads <- as.integer(args[2])
 
-sink(file = file.path(destdir, "mirror.log"))
+sink(file = file.path(destdir, "mirror.txt"))
+on.exit(sink(NULL))
 message("destdir: ", destdir)
 message("threads: ", threads)
 
 if (!dir.exists(destdir)) dir.create(destdir, recursive = TRUE)
-future::plan(multisession, workers = threads)
+if (threads != 0) {
+    future::plan(multisession, workers = threads)
+    call_fun <- furrr::future_map
+} else {
+    call_fun <- purrr::map
+}
 
 download_dataset <- function(x, destdir) {
     x %>%
@@ -41,21 +48,18 @@ download_dataset <- function(x, destdir) {
 access_datasets <- XenaData # XenaDataUpdate(saveTolocal = FALSE)
 hubs <- unique(access_datasets$XenaHostNames)
 
-dataset_list <- furrr::future_map(hubs, function(h) {
-    sink(file = file.path(destdir, "mirror.log"), append = TRUE)
+dataset_list <- call_fun(hubs, function(h) {
+    sink(file = file.path(destdir, "mirror.txt"), append = TRUE)
     x <- subset(access_datasets, XenaHostNames == h)
     message("Mirroring hub: ", h)
     message("================================")
     download_dataset(x, file.path(destdir, h))
-},
-.progress = TRUE
-)
+})
 
 mapping_df <- Reduce(rbind, dataset_list)
 
 message("================================")
-message("The mirror work has done, please check the log.")
-sink()
+message("The mirror work has done, please check the mirror.txt.")
 
 write.csv(mapping_df,
           file = file.path(destdir, "mapping_dataframe.csv"),
