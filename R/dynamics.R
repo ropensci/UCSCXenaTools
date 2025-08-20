3aef3fe5701a194a761466af544f60cdd102574d
Shixiang Wang (王诗翔) <wangshx@shanghaitech.edu.cn>
2025-08-20 11:20:06 +0800

    add doc for dynamics

---
+++ R/dynamics.R
@@ -0,0 +1,43 @@
+#' UCSC Xena Dynamic Objects
+#'
+#' A collection of dynamically generated objects for accessing UCSC Xena data.
+#' These objects are created when the package is loaded via `.onAttach`.
+#'
+#' @format Various formats depending on the object (e.g., characters, or functions).
+#' @source UCSC Xena API
+#' @name UCSCXenaTools-dynamic
+#' @aliases .p_all_cohorts .p_all_datasets .p_all_datasets_n
+#' .p_all_field_metadata .p_cohort_samples .p_cohort_summary
+#' .p_dataset_fetch .p_dataset_field .p_dataset_field_examples
+#' .p_dataset_field_n .p_dataset_gene_probe_avg
+#' .p_dataset_gene_probes_values .p_dataset_list
+#' .p_dataset_metadata .p_dataset_probe_signature
+#' .p_dataset_probe_values .p_dataset_samples
+#' .p_dataset_samples_ndense_matrix .p_dataset_sources
+#' .p_dataset_status .p_datasets_null_rows .p_feature_list
+#' .p_field_codes .p_field_metadata .p_gene_transcripts
+#' .p_match_fields .p_probe_count .p_probemap_list
+#' .p_ref_gene_exons .p_ref_gene_position .p_ref_gene_range
+#' .p_segment_data_examples .p_segmented_data_range .p_sparse_data
+#' .p_sparse_data_examples .p_sparse_data_match_field
+#' .p_sparse_data_match_field_slow
+#' .p_sparse_data_match_partial_field .p_sparse_data_range
+#' .p_transcript_expression .xq_all_cohorts .xq_all_datasets
+#' .xq_all_datasets_n .xq_all_field_metadata .xq_cohort_samples
+#' .xq_cohort_summary .xq_dataset_fetch .xq_dataset_field
+#' .xq_dataset_field_examples .xq_dataset_field_n
+#' .xq_dataset_gene_probe_avg .xq_dataset_gene_probes_values
+#' .xq_dataset_list .xq_dataset_metadata
+#' .xq_dataset_probe_signature .xq_dataset_probe_values
+#' .xq_dataset_samples .xq_dataset_samples_ndense_matrix
+#' .xq_dataset_sources .xq_dataset_status .xq_datasets_null_rows
+#' .xq_feature_list .xq_field_codes .xq_field_metadata
+#' .xq_gene_transcripts .xq_match_fields .xq_probe_count
+#' .xq_probemap_list .xq_ref_gene_exons .xq_ref_gene_position
+#' .xq_ref_gene_range .xq_segment_data_examples
+#' .xq_segmented_data_range .xq_sparse_data
+#' .xq_sparse_data_examples .xq_sparse_data_match_field
+#' .xq_sparse_data_match_field_slow
+#' .xq_sparse_data_match_partial_field .xq_sparse_data_range
+#' .xq_transcript_expression
+NULL
